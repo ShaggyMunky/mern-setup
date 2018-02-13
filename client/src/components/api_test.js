1c341e81cddac1b592ff5b9b80b4e89cab927904
ShaggyMunky <aaronguillen21@gmail.com>
2018-02-12 16:05:55 -0800

    API setup complete

--- client/src/components/api_test.js
+++ client/src/components/api_test.js
@@ -1,10 +1,31 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class ApiTest extends Component {
+
+    getUserData(){
+        axios.get("/api/get-user").then((resp) =>{
+            console.log("response from get user: ", resp);
+        }).catch((err) => {
+            console.log("ERROR from get user: ", err)
+        });
+    }
+
+    getArticleData(){
+        axios.get("/api/get-articles").then((resp) => {
+            console.log("response articles: ", resp);
+        }).catch((err) => {
+            console.log("ERROR get-articles: ", err.message)
+        });
+    }
+
     render(){
         return (
             <div>
                 <h1>This is API</h1>
+                <button onClick={this.getUserData}>Get User Info</button>
+                <br/>
+                <button onClick={this.getArticleData}>Get Article Data</button>
             </div>
         );
     }
